f96f502b59fb56133b732afaece3935b4a0bd605
Nikkoro <90435784+id-Nick010@users.noreply.github.com>
2025-06-27 08:37:43 +0800

    feat(router): integrate React Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-// import './App.css'
 import WelcomePage from './pages/WelcomePage.tsx';
+import {Routes, Route} from 'react-router-dom'
 
 function App() {
 
   return (
-    <>
-      <WelcomePage />
-    </>
+    <Routes>
+      <Route path='/' element={<WelcomePage/>} />
+    </Routes>
   )
 }
 
