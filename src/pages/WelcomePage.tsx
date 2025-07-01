77db24598e28cc2e12810c9aed0b60800b0bdf31
Nikkoro <90435784+id-Nick010@users.noreply.github.com>
2025-07-01 22:29:00 +0800

    chore: relocate some tailwind from tsx to css

--- src/pages/WelcomePage.tsx
+++ src/pages/WelcomePage.tsx
@@ -1,13 +1,14 @@
 import {useState} from 'react'
 import starlinkedLogo from '../assets/starlinked_logo.svg'
 import Modal from '../components/Modal'
+import '../styles/welcomepage.css'
 
 function WelcomePage () {
     const [isModalOpen, setModalOpen] = useState(false);
     
     return (
-        <div className=" font-sarabun flex flex-col items-center justify-center h-screen">
-            <div className='flex flex-row w-full justify-between'>
+        <div className="welcome-container">
+            <div className='flex flex-row w-md justify-between'>
                 <h2 className='font-sarabun self-start text-4xl italic w-2/4'>Welcome to</h2>
                 <div className=' h-1 bg-white my-4 w-2/4'></div>
             </div>
@@ -15,12 +16,11 @@ function WelcomePage () {
             <Modal isOpen={isModalOpen} onClose={() => setModalOpen(false)}>
                 <h2 className="text-lg text-gray-700 font-bold">Hello, Nickolo!</h2>
                 <p className='text-gray-600'>This is where you type the code link for a study session that's already created.</p>
-                
             </Modal>
-            <div className='flex flex-col space-y-3 mt-20'>
-                <button onClick={()=> setModalOpen(true)} className='px-5 font-sarabun bg-transparent text-3xl'>join a space</button>
-                <button className='px-5 bg-transparent text-3xl'>create a space</button>
-                <button className='px-5 bg-transparent text-3xl'>about</button>
+            <div className='wel-button-container'>
+                <button onClick={()=> setModalOpen(true)} className='wel-button'>join a space</button>
+                <button className='wel-button'>create a space</button>
+                <button className='wel-button'>about</button>
             </div>
         </div>
     );
