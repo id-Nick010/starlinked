58acd65cec20894e5ccfe034a453f06dc8bd4504
Nikkoro <90435784+id-Nick010@users.noreply.github.com>
2025-07-01 21:28:55 +0800

    refac: fix font import & disable default css

--- src/pages/WelcomePage.tsx
+++ src/pages/WelcomePage.tsx
@@ -6,7 +6,7 @@ function WelcomePage () {
     const [isModalOpen, setModalOpen] = useState(false);
     
     return (
-        <div className=" flex flex-col items-center justify-center h-screen">
+        <div className=" font-sarabun flex flex-col items-center justify-center h-screen">
             <div className='flex flex-row w-full justify-between'>
                 <h2 className='font-sarabun self-start text-4xl italic w-2/4'>Welcome to</h2>
                 <div className=' h-1 bg-white my-4 w-2/4'></div>
@@ -19,8 +19,8 @@ function WelcomePage () {
             </Modal>
             <div className='flex flex-col space-y-3 mt-20'>
                 <button onClick={()=> setModalOpen(true)} className='px-5 font-sarabun bg-transparent text-3xl'>join a space</button>
-                <button className='px-5 font-sarabun bg-transparent text-3xl'>create a space</button>
-                <button className='px-5 font-sarabun bg-transparent text-3xl'>about</button>
+                <button className='px-5 bg-transparent text-3xl'>create a space</button>
+                <button className='px-5 bg-transparent text-3xl'>about</button>
             </div>
         </div>
     );
