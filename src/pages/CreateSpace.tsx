bb986d4f4f677e9feb27df47cef381f4ca8e4d28
Nikkoro <90435784+id-Nick010@users.noreply.github.com>
2025-07-05 00:45:41 +0800

    feat(nav): add return to home navigation in create space screen

--- src/pages/CreateSpace.tsx
+++ src/pages/CreateSpace.tsx
@@ -1,7 +1,12 @@
+import DefaultButton from "../components/DefaultButton";
+import { useNavigate } from "react-router-dom";
+
 function CreateSpace() {
+    const navigate = useNavigate();
     return (
-        <div className="flex justify-center items-center w-screen h-screen">
+        <div className="flex flex-col justify-center items-center w-screen h-screen">
             <h1>Create Space Page</h1>
+            <DefaultButton textSize="md" onClick={() => navigate('/')} label='Go back'/>
         </div>
     )
 }
