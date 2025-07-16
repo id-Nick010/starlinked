a386f2bde60c3ac5447f3b7f18d46addde326e3c
Nikkoro <90435784+id-Nick010@users.noreply.github.com>
2025-07-17 00:02:23 +0800

    feat(create): style initial layout structure for create space

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -4,7 +4,7 @@ import { useNavigate } from 'react-router-dom'
 export default function NavBar () {
     const navigate = useNavigate();
     return (
-        <section className='flex justify-start items-start w-screen h-15 m-5'>
+        <section className='top-0 fixed flex justify-start items-start w-full h-15 m-5'>
             <button onClick={() => navigate('/')}>
                 <img src={starlinkedLogo} alt='starlinked logo button' className='h-10 w-auto ml-5'/>
             </button>
--- src/pages/CreateSpace.tsx
+++ src/pages/CreateSpace.tsx
@@ -2,22 +2,17 @@ import NavBar from "../components/NavBar";
 
 function CreateSpace() {
     return (
-        <div className="flex flex-col justify-start items-center w-screen h-screen">
-            <NavBar />
-            <h1>Create Space Page</h1>
-            <section className=''>
-                <h2>What do you want to name your space?</h2>
-                <input type='text' placeholder='Please enter your space name...'/>
-            </section>
-            <section className="">
-                <h2>That's a good one!<br/>what's your name btw?</h2>
-                <input type='text' placeholder='Please enter your name...'/>
-            </section>
-            <section className="">
-                <h2>Nice to meet you your-name-here !</h2>
-                <h2>Please wait for a moment...</h2>
-                <h2>I'm done creating your space. Enjoy your stay~</h2>
-            </section>
+        <div className="flex flex-col justify-center items-center w-screen h-screen">
+            <NavBar/>
+            <main className='grid grid-col-2 gap-10 text-3xl w-3/4 text-gray-300 font-sarabun font-light'>
+                <h2 className=''>What do you want to name your space?</h2>
+                <input className="col-start-2 row-start-2 outline-none" type='text' placeholder='Please enter your space name...'/>
+                <h2 className="row-start-3">That's a good one!<br/>what's your name btw?</h2>
+                <input className='col-start-2 row-start-4 outline-none' type='text' placeholder='Please enter your name...'/>
+                <h2 className="row-start-5">Nice to meet you your-name-here !</h2>
+                <h2 className="row-start-6">Please wait for a moment...</h2>
+                <h2 className="row-start-7">I'm done creating your space. Enjoy your stay~</h2>
+            </main>
         </div>
     )
 }
